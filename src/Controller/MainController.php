48417cb07e45423eb6df1917b52bf76bafd28d72
Dana Popa <popa.dana100@gmail.com>
2024-04-19 13:19:27 +0300

    page building (route, controller and response)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function homepage() 
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+    }
+}
+
+?>
